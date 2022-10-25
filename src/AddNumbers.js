4c6bc3e4e85e2fe3499a3c99146a9da17fe1553f
Kelsey Greene <kelsey.greene17@gmail.com>
2022-10-24 21:04:28 -0500

    state and handler functions created

--- src/AddNumbers.js
+++ src/AddNumbers.js
@@ -1,20 +1,40 @@
 import React, { useState } from 'react';
 
  export const AddNumbers = () => {
-    const [count, setCount] = useState(null);
+    //create my needed states
+    const [val1, setVal1] = useState(null);
+    const [val2, setVal2] = useState(null);
+    const [total, setTotal] = useState(null);
 
+    //create funcs to track input and add/display total
+
+    const handleInput1 = (event) => {
+        setVal1(event.target.value);
+    };
+    const handleInput2 = (event) => {
+        setVal2(event.target.value);
+    }
+    const addNums = (val1, val2) => {
+        //using parseInt here to convert to Integer so that eg 3+4 returns '7' and not '34'
+        setTotal(parseInt(val1) + parseInt(val2))
+    }
+
+    //
     return (
         <div>
             <h1> American Airlines Add Numbers Exercise </h1>
-            <input
+            <input onChange={(event) => handleInput1(event)}
+                value={val1 ? val1 : ''}
             />
             +
-            <input
+            <input onChange={(event) => handleInput2(event)}
+                value={val2 ? val2 : ''}
             />
             <br/>
-            <button>
+            <button onClick={() => addNums(val1, val2)}>
                 Total
             </button>
+            <p>{total} </p>
         </div>
     )
 } 
